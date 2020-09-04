da3f65e1acef372670e93bec67b7641ce1d861c7
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-09-04 08:58:01 +0800

    创建订单模块的数据模型

---
+++ 13-book-step3/src/order.sql
@@ -0,0 +1,20 @@
+USE book;
+
+CREATE TABLE `order` (
+    order_id VARCHAR(50) PRIMARY KEY,
+    create_time DATETIME NOT NULL,
+    price DECIMAL(11,2) NOT NULL,
+    status INT NOT NULL,
+    user_id INT NOT NULL,
+    CONSTRAINT fk_order_user FOREIGN KEY(user_id) REFERENCES user(id)
+);
+
+CREATE TABLE order_item (
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    name VARCHAR(100) NOT NULL ,
+    count INT NOT NULL ,
+    price DECIMAL(11,2) NOT NULL ,
+    totalPrice DECIMAL(11,2) NOT NULL ,
+    order_id VARCHAR(50),
+    CONSTRAINT fk_order_item_order FOREIGN KEY (order_id) REFERENCES `order`(order_id)
+);
